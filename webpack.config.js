197b10d346f8586b00d1429d665dc83e840c6279
manueldelapenna <manueldelapenna@gmail.com>
2025-02-10 10:50:55 -0300

    con @areanor dejamos instalado webpack y babel para empezar a generar el bundle

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: {
+        "frontend-plus": './dist/lib/frontend-plus.js',
+        //unlogged:'./dist/unlogged/unlogged.js',
+    },
+    output: {
+        filename: 'dist/[name]-bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
