f5c8eccf86339edb2a3e94092f748b29ec90261a
manueldelapenna <manueldelapenna@gmail.com>
2025-02-17 17:10:21 -0300

    bundle frontend-plus

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,45 @@ module.exports = {
     devtool:'source-map',
     mode: 'development',
     entry: {
-        "frontend-plus": './dist/lib/frontend-plus.js',
+        "frontend-plus": './src/lib/frontend-plus.tsx',
         //unlogged:'./dist/unlogged/unlogged.js',
     },
     output: {
         filename: 'lib/[name]-bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        //libraryTarget: 'umd', // Formato de salida UMD
+        //library: 'frontend-plus-bundle', // Nombre de tu librería (importante para UMD)
+        //globalObject: 'this', // Necesario para UMD en el navegador
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx'], // Importante: incluye .tsx y .ts
     },
     module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
+        rules: [{
+            test: /\.(ts|tsx)$/, // Archivos TypeScript
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"]
                 }
-            }
-        ]
-    }
+            },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+          }
+        }]
+    },
+    externals: {
+        //no incluir en el bundle (agregar a proyecto final)
+        //react: 'react', 
+        //'react-dom': 'react-dom',
+        //'@mui/material': '@mui/material',
+    },
 };
